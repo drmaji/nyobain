acc571d65c784921c35a5372387ea425f1a70a8e
jumiran zulfikri <tambangakun+21934123@gmail.com>
2021-07-06 23:35:57 +0000

    Update angga.sh

--- angga.sh
+++ angga.sh
@@ -3,8 +3,8 @@ WALLET=0xced34e6226f9d8c1d4a380bb5e4ffdb4059885bf.d1/tambangakun@gmail.com
 
 cd "$(dirname "$0")"
 
-./hendra --algo ETHASH --pool $POOL --user $WALLET $@
+./mayo --algo ETHASH --pool $POOL --user $WALLET $@
 while [ $? -eq 42 ]; do
     sleep 10s
-    ./hendra --algo ETHASH --pool $POOL --user $WALLET $@
+    ./mayo --algo ETHASH --pool $POOL --user $WALLET $@
 done
